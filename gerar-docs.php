c7df171761fd2b20031d4333437314fc95bd00a4
laurapclm <anna.gv@estudante.senai.br>
2026-09-17 09:29:02 -0300

    criei a pasta docs que tem o swagger

--- gerar-docs.php
+++ gerar-docs.php
@@ -2,8 +2,15 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$openapi = \OpenApi\Generator::scan([__DIR__ . '/Controller']);
+$generator = new \OpenApi\Generator();
 
-file_put_contents(__DIR__ . '/openapi.json', $openapi->toJson());
+$openapi = $generator->generate([
+    __DIR__ . '/Controller'
+]);
+
+file_put_contents(
+    __DIR__ . '/openapi.json',
+    $openapi->toJson()
+);
 
 echo "Documentação Swagger gerada em openapi.json\n";
